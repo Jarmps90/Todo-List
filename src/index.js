f6d51148588979f344fc99d6ab6e9c484e6e2936
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2025-12-17 19:41:41 +0200

    Created two functions and added some instructions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import { removeTodo } from './objects.js';
+
+
+
+
+function domControll() {
+//Gomes two functions one that renders todos
+// and one that renders user interface
+};
+
+function userInput() {
+//Function that takes and sends user inputs to object
+// creation
+};
