7ef9318364df42c3254138520ec612f8fc009b4c
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2025-12-17 16:27:57 +0200

    Created function for creating objects, and created remove function

--- src/objects.js
+++ src/objects.js
@@ -1,20 +1,35 @@
 
-class Todo {
-  constructor(title, description, duedate, priority) {
-    this.title = title;
-    this.description = description;
-    this.duedate = duedate;
-    this.priority = priority;
+
+function createTodo(title, description, duedate, priority) {
+
+  class Todo {
+    constructor(title, description, duedate, priority) {
+      this.title = title;
+      this.description = description;
+      this.duedate = duedate;
+      this.priority = priority;
+    };
   };
+
+  const todoToLocalStorage = JSON.stringify(new Todo(title, description, duedate, priority));
+
+  localStorage.setItem(title, todoToLocalStorage);
+
+  const todoFromLocalStorage = JSON.parse(localStorage.getItem(title));
+
+  console.log(todoFromLocalStorage);
 };
 
 
-const newTodo = new Todo('test', 'test text that does not matter', '12.12.2046', 'High');
 
-let todoToLocalStorage = JSON.stringify(newTodo);
 
-localStorage.setItem('todo', todoToLocalStorage);
 
-let todoFromLocalStorage = JSON.parse(localStorage.getItem('todo'));
+createTodo('building', 'Building house', '12.12.2025', 'Medium' );
+createTodo('workout', 'running till legs fall of', '11.12.2025', 'low');
 
-console.log(todoFromLocalStorage);
+export function removeTodo() {
+  let title = localStorage.getItem(title);
+
+  console.log(title);
+  // localStorage.removeItem(title);
+};
