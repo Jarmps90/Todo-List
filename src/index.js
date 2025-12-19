8e984f40f1df00ef03ae621a1a1dc4bda86c1928
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2025-12-19 20:42:19 +0200

    Changed structure of modal function

--- src/index.js
+++ src/index.js
@@ -2,25 +2,34 @@ import { removeTodo } from './objects.js';
 
 
 
-function modal() {
+const modal = (function() {
   const container = document.querySelector('#container');
-  const dialog = document.createElement('dialog');
-  const addBtn = document.createElement('button');
 
-  dialog.classList.add('modal');
-  addBtn.classList.add('addBtn');
-  addBtn.textContent = 'Add';
-  container.appendChild(addBtn);
+  const modalCreate = () => {
+    const dialog = document.createElement('dialog');
 
-  addBtn.addEventListener('click', () => {
+    dialog.classList.add('modal');
     container.appendChild(dialog);
     dialog.showModal();
-  });
+  };
+
+  const createTodoBtn = () => {
+    const addBtn = document.createElement('button');
+
+    addBtn.classList.add('addBtn');
+    addBtn.textContent = 'Add';
+    container.appendChild(addBtn);
+
+    addBtn.addEventListener('click', () => {
+      modalCreate()
+
+    });
+  };
+
+  return {modalCreate, createTodoBtn}
+});
 
 
-
-
-};
 
 function domControll() {
 
