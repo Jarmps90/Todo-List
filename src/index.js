c2619a36edb80ced0f7d67136b050cd0e8b4ade2
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2025-12-26 12:18:19 +0200

    Creating factory functions instead of class, seems simler

--- src/index.js
+++ src/index.js
@@ -5,27 +5,38 @@
 const modal = (function() {
   const container = document.querySelector('#container');
 
-  class DomElementCreate {
-    constructor(id, type) {
-      this.type = type;
-      this.id = id;
+  // class DomElementCreate {
+  //   constructor(id, type) {
+  //     this.type = type;
+  //     this.id = id;
 
-    };
+  //   };
 
 
 
-    get labelElemnts() {
-      const labelEl = document.createElement('label');
-      const inputEl = document.createElement('input');
+  //   get labelElemnts() {
+  //     const labelEl = document.createElement('label');
+  //     const inputEl = document.createElement('input');
 
-      labelEl.htmlFor = this.id;
-      inputEl.id = this.id
+  //     labelEl.htmlFor = this.id;
+  //     inputEl.id = this.id
 
-      return labelEl
-    };
+  //     return labelEl
+  //   };
 
+
+  // };
+  function InputElements(type, id) {
 
   };
+
+  function LabelElements(labelFor) {
+    const el = document.createElement('label');
+    el.htmlFor = labelFor;
+
+    return container.appendChild(el);
+  };
+
 
   const modalCreate = () => {
     let labelElement = new DomElementCreate('title');
