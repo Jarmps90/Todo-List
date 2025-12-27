3b94cd52bb0a21054aa53b39df0bf02595a8df30
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2025-12-27 12:14:47 +0200

    Created new FF to create options, changed some appendings, added some options to modal

--- src/index.js
+++ src/index.js
@@ -6,31 +6,57 @@ const modal = (function() {
   const container = document.querySelector('#container');
   const dialog = document.createElement('dialog');
   const form = document.createElement('form');
+  const prioritySelect = document.createElement('select');
 
+  //To create input elements
   function InputElements(type, id, name) {
     const el = document.createElement('input');
     el.type = type;
     el.id = id;
     el.name = name;
 
-    return container.appendChild(el);
+    return form.appendChild(el);
   };
 
-
+  //To create label elements
   function LabelElements(labelFor, text) {
     const el = document.createElement('label');
     el.htmlFor = labelFor;
     el.textContent = text;
 
-    return container.appendChild(el);
+    return form.appendChild(el);
+  };
+
+  //To create options for select
+  function CreateSelect(value, content) {
+    const el = document.createElement('option');
+    el.value = value;
+    el.textContent = content;
+
+    return prioritySelect.appendChild(el);
   };
 
 
   const modalCreate = () => {
+
     const titleLabel = LabelElements('title', 'Title:')
     const titleInput = InputElements('text', 'title', 'title');
     const textLabel = LabelElements('description', 'Desctioption:');
     const textInput = InputElements('text', 'description', 'description');
+    const dueDateLabel = LabelElements('dueDate', 'Due date:');
+    const dueDateInput = InputElements('date', 'dueDate', 'dueDate');
+    const priorityLabel = LabelElements('priority-select', 'Select priority:');
+    const selectPriority = CreateSelect("''", 'Please choose an option');
+    const lowPrioriy = CreateSelect('low', 'Low');
+    const mediumPriority = CreateSelect('medium', 'Medium');
+    const highPriority = CreateSelect('high', 'High');
+
+    dialog.id = 'todoModal';
+    prioritySelect.id = 'priority-select';
+    prioritySelect.name = 'priority';
+    form.appendChild(prioritySelect);
+    container.appendChild(dialog);
+    dialog.appendChild(form);
   };
 
   const createTodoBtn = () => {
@@ -41,8 +67,9 @@ const modal = (function() {
     container.appendChild(addBtn);
 
     addBtn.addEventListener('click', () => {
-
-
+      dialog.textContent = "";
+      modalCreate();
+      dialog.showModal();
     });
   };
 
