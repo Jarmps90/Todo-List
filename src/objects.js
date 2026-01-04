438e85d1be16d7974e51fcbae0d65267e9e9127c
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2026-01-04 12:01:26 +0200

    created function that gets strings from localStorage converts them objects and pushes to array

--- src/objects.js
+++ src/objects.js
@@ -12,18 +12,15 @@ export function createTodo(title, description, duedate, priority) {
   };
 
   const todoToLocalStorage = JSON.stringify(new Todo(title, description, duedate, priority));
-
   localStorage.setItem(title, todoToLocalStorage);
 
-  const todoFromLocalStorage = JSON.parse(localStorage.getItem(title));
-
-  console.log(todoFromLocalStorage);
 };
 
+export function todoControl(key) {
+  let todoArray = [];
+  const stringsToObj = todoArray.push(JSON.parse(localStorage.getItem(key)));
 
+  const getTodoArray = () => todoArray;
 
-
-
-function removeTodo() {
-
+  return { getTodoArray};
 };
