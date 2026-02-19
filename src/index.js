553358b179dc909c73e6908c9d9a224b9f486c4b
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2026-02-19 10:44:53 +0200

    Removed unused function calls

--- src/index.js
+++ src/index.js
@@ -1,145 +1,137 @@
-import { objectControl } from './objects.js';
-import { modal } from './modal.js'
-import './style.css';
-
+import { objectControl } from "./objects.js";
+import { modal } from "./modal.js";
+import "./style.css";
 
 modal.createProjectBtn();
 modal.createTodoBtn();
 
 const navBar = (function () {
-  const container = document.querySelector('#container');
-  const navBar = document.createElement('nav');
+  const container = document.querySelector("#container");
+  const navBar = document.createElement("nav");
 
-  navBar.classList.add('projec-bar');
+  navBar.classList.add("projec-bar");
   container.appendChild(navBar);
-}());
-
+})();
 
 const objects = objectControl();
 
-
 export function projectId() {
-  const navBar = document.querySelector('nav');
+  const navBar = document.querySelector("nav");
   let projectId = 0;
 
-  navBar.addEventListener('click', (event) => {
+  navBar.addEventListener("click", (event) => {
     projectId = event.target.id;
   });
 
   const getPrjectId = () => projectId;
 
   return { getPrjectId };
-};
-
-
+}
 
 function updateDisplay() {
-  let key = Object.keys(localStorage)
+  let key = Object.keys(localStorage);
 
   if (localStorage.getItem(key) !== null) {
     projectDisplay();
   } else {
-    console.log('Storage is empty');
-  };
-};
-
+    console.log("Storage is empty");
+  }
+}
 
 function todoDisplay() {
-    const container = document.querySelector('#container');
+  const container = document.querySelector("#container");
 
-    // const todo = todoControl();
-    const todoArray = todo.getTodoArray();
-    container.innerHTML = "";
+  // const todo = todoControl();
+  const todoArray = [];
 
-    for (let i = 0; i < todoArray.length; i++) {
-        const todoCard = document.createElement('div');
-        todoCard.id = 'todoCard';
+  for (let i = 0; i < todoArray.length; i++) {
+    const todoCard = document.createElement("div");
+    todoCard.id = "todoCard";
 
-        const title = document.createElement('p');
-        const description = document.createElement('p');
-        const dueDate = document.createElement('p');
-        const priority = document.createElement('p');
+    const title = document.createElement("p");
+    const description = document.createElement("p");
+    const dueDate = document.createElement("p");
+    const priority = document.createElement("p");
 
-        title.innerText = `Title: ${todoArray[i].title}`;
-        description.innerText = `Description: ${todoArray[i].description}`;
-        dueDate.innerText = `Due Date: ${todoArray[i].dueDate}`;
-        priority.innerText = `Priority: ${todoArray[i].priority}`;
+    title.innerText = `Title: ${todoArray[i].title}`;
+    description.innerText = `Description: ${todoArray[i].description}`;
+    dueDate.innerText = `Due Date: ${todoArray[i].dueDate}`;
+    priority.innerText = `Priority: ${todoArray[i].priority}`;
 
-        todoCard.appendChild(title);
-        todoCard.appendChild(description);
-        todoCard.appendChild(dueDate);
-        todoCard.appendChild(priority);
+    todoCard.appendChild(title);
+    todoCard.appendChild(description);
+    todoCard.appendChild(dueDate);
+    todoCard.appendChild(priority);
 
-        container.appendChild(todoCard);
-      };
-};
+    container.appendChild(todoCard);
+  }
+}
 
 function projectDisplay() {
-  const navBar = document.querySelector('nav');
+  const navBar = document.querySelector("nav");
   const projectArray = objects.getProjects();
   let count = 0;
   navBar.innerHTML = "";
 
   projectArray.forEach((element) => {
-      const objectDiv = document.createElement('div');
+    const objectDiv = document.createElement("div");
 
-      objectDiv.textContent = element.projectName;
-      objectDiv.id = count++;
-      navBar.appendChild(objectDiv);
-    });
-
-};
-
-
+    objectDiv.textContent = element.projectName;
+    objectDiv.id = count++;
+    navBar.appendChild(objectDiv);
+  });
+}
 
 export function userInput() {
-  const container = document.querySelector('#container');
-
+  const container = document.querySelector("#container");
 
   const submit = () => {
+    container.addEventListener("click", function addTodo(event) {
+      if (event.target.id === "submitBtn") {
+        const dialog = document.querySelector("#todoModal");
+        const todoTitle = document.getElementById("title").value;
+        const todoDescription = document.getElementById("description").value;
+        const todoDueDate = document.getElementById("dueDate").value;
+        const todoPriority = document.getElementById("priority-select").value;
 
-    container.addEventListener('click', function addTodo(event) {
-      if (event.target.id === 'submitBtn') {
-        const dialog = document.querySelector('#todoModal');
-        const todoTitle = document.getElementById('title').value;
-        const todoDescription = document.getElementById('description').value;
-        const todoDueDate = document.getElementById('dueDate').value;
-        const todoPriority = document.getElementById('priority-select').value;
-
-        objects.todoControl(todoTitle, todoDescription, todoDueDate, todoPriority);
+        objects.todoControl(
+          todoTitle,
+          todoDescription,
+          todoDueDate,
+          todoPriority,
+        );
         event.preventDefault();
         dialog.close();
         dialog.remove();
-        container.removeEventListener('click', addTodo);
-      };
+        container.removeEventListener("click", addTodo);
+      }
     });
   };
 
   const projectSubmit = () => {
-    container.addEventListener('click',  function addProject(event) {
-
-      container.addEventListener('keydown', (event) => {
-        if (event.key === 'Escape') {
-         const dialog = document.querySelector('dialog');
-         dialog.close();
-         dialog.remove();
-         container.removeEventListener('click', addProject)
-         };
+    container.addEventListener("click", function addProject(event) {
+      container.addEventListener("keydown", (event) => {
+        if (event.key === "Escape") {
+          const dialog = document.querySelector("dialog");
+          dialog.close();
+          dialog.remove();
+          container.removeEventListener("click", addProject);
+        }
       });
 
-      if (event.target.id === 'projectSubmitBtn') {
-        const dialog = document.querySelector('#projectModalDialog');
-        const prjectName = document.getElementById('project').value;
+      if (event.target.id === "projectSubmitBtn") {
+        const dialog = document.querySelector("#projectModalDialog");
+        const prjectName = document.getElementById("project").value;
 
         objects.projectControl(prjectName);
         projectDisplay();
         event.preventDefault();
         dialog.close();
         dialog.remove();
-        container.removeEventListener('click', addProject);
-      };
+        container.removeEventListener("click", addProject);
+      }
     });
   };
   return { submit, projectSubmit };
-};
+}
 updateDisplay();
