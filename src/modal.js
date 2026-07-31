7871ceb0e6ba2a3da948ee55e09d3bbdca6cf69a
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2026-07-31 08:20:45 +0300

    Updated

--- src/modal.js
+++ src/modal.js
@@ -83,7 +83,6 @@ export const modal = (function() {
   const createProjectBtn = () => {
     const projectModalBtn = document.createElement('button');
     const buttonDiv = document.querySelector('nav');
-    console.log(buttonDiv);
 
     projectModalBtn.id = 'projectModal';
     projectModalBtn.textContent = 'Create project';
