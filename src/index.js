afa3d0bcd67f594a0c699f3cbed9143f9784a06e
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2026-09-11 08:48:18 +0300

    Creating filter function that filters out completed todos

--- src/index.js
+++ src/index.js
@@ -104,11 +104,14 @@ function todoChecker() {
 
 function doneCheck() {
   const todos = objects.getTodos();
-
+  const checkbox = document.getElementById('toggle-btn');
+  
   todos.forEach((todo) => {
    if(todo.completed === true) {
+      console.log(todo.id);
    };
   });
+  console.log(todos);
 };
 
 
